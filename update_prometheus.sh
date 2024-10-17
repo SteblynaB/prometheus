2034ebac44f764ee802e2390ca33fd4a483b059f
SteblynaB <120423885+SteblynaB@users.noreply.github.com>
2024-10-17 23:53:13 +0300

    Update update_prometheus.sh

--- update_prometheus.sh
+++ update_prometheus.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+#Удаляем node_exporter
+sudo systemctl stop node_exporter && systemctl disable node_exporter
+sudo apt-get remove prometheus-node-exporter
+sudo apt-get purge prometheus-node-exporter
+sudo apt-get autoremove
+
+#Удаляем prometheus
+sudo systemctl stop prometheus && systemctl disable prometheus
+sudo apt-get remove prometheus
+sudo rm -rf /etc/prometheus /var/lib/prometheus
+sudo apt-get autoremove
+
+#Устанавливаем node_exporter
+sudo apt-get update
+sudo apt-get install prometheus-node-exporter -y
+sudo systemctl enable node_exporter && sudo systemctl restart node_exporter
+
 # Проверка, существует ли SSH-ключ
 if [ ! -f ~/.ssh/id_rsa ]; then
     echo "SSH-ключ не найден. Создание нового ключа..."

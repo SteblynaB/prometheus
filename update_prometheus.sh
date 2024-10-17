dc1d4c46adf5d66cf4364ad9d782d008ac8567b4
SteblynaB <120423885+SteblynaB@users.noreply.github.com>
2024-10-17 23:29:20 +0300

    Update update_prometheus.sh

--- update_prometheus.sh
+++ update_prometheus.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
 
-# Проверка, существует ли SSH-ключ
-if [ ! -f ~/.ssh/id_rsa ]; then
-    echo "SSH-ключ не найден. Создание нового ключа..."
-    ssh-keygen -t rsa -b 4096 -N "" -f ~/.ssh/id_rsa
-    echo "Ключ создан."
-fi
-
-# Копирование SSH-ключа на удаленный сервер
-echo "Копирование SSH-ключа на удаленный сервер..."
-ssh-copy-id -o StrictHostKeyChecking=no root@65.109.134.69
-
-# Получение IP-адреса текущего сервера
 SERVER_IP=$(hostname -I | awk '{print $1}')
 read -p "Введите SERVER_NAME: " SERVER_NAME
 
-# Подключение к удаленному серверу и добавление конфигурации
-ssh -o StrictHostKeyChecking=no root@65.109.134.69 << EOF
+ssh root@65.109.134.69 << EOF
 {
   echo "      - targets:"
   echo "          - '$SERVER_IP:9100'"
@@ -25,5 +12,3 @@ ssh -o StrictHostKeyChecking=no root@65.109.134.69 << EOF
   echo "          nodename: '$SERVER_NAME'"
 } | sudo tee -a /etc/prometheus/prometheus.yml
 EOF
-
-echo "Конфигурация успешно обновлена!"

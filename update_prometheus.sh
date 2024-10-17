db88ca61890ef3fee875359b79547e848b50037d
SteblynaB <120423885+SteblynaB@users.noreply.github.com>
2024-10-17 23:09:35 +0300

    Update update_prometheus.sh

--- update_prometheus.sh
+++ update_prometheus.sh
@@ -3,10 +3,12 @@
 read -p "Введите SERVER_IP: " SERVER_IP
 read -p "Введите SERVER_NAME: " SERVER_NAME
 
+ssh root@65.109.134.69 << EOF
 {
   echo "      - targets:"
   echo "          - '$SERVER_IP:9100'"
   echo "        labels:"
   echo "          instance: '$SERVER_IP'"
   echo "          nodename: '$SERVER_NAME'"
-} >> /etc/prometheus/prometheus.yml
+} | sudo tee -a /etc/prometheus/prometheus.yml
+EOF

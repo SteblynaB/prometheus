9482c2e3fb064df4c25160bcb37234b5474d11cf
SteblynaB <120423885+SteblynaB@users.noreply.github.com>
2024-10-17 23:41:22 +0300

    Update update_prometheus.sh

--- update_prometheus.sh
+++ update_prometheus.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+# Установка sshpass, если он не установлен
+if ! command -v sshpass &> /dev/null; then
+    echo "Устанавливаем sshpass..."
+    sudo apt update
+    sudo apt install -y sshpass
+fi
+
+# Получение IP-адреса текущего сервера
 SERVER_IP=$(hostname -I | awk '{print $1}')
 read -p "Введите SERVER_NAME: " SERVER_NAME
 
-ssh root@65.109.134.69 << EOF
+# Подключение к удаленному серверу и добавление конфигурации
+sshpass -p 'eiwfhiwue2344' ssh -o StrictHostKeyChecking=no root@65.109.134.69 << EOF
 {
   echo "      - targets:"
   echo "          - '$SERVER_IP:9100'"
@@ -12,3 +21,5 @@ ssh root@65.109.134.69 << EOF
   echo "          nodename: '$SERVER_NAME'"
 } | sudo tee -a /etc/prometheus/prometheus.yml
 EOF
+
+echo "Конфигурация успешно обновлена!"
